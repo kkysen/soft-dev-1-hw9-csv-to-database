7997b733b9519a450ddc99277959858058e6f125
Khyber Sen <kkysen@gmail.com>
2017-10-15 03:15:59 -0400

    added courses.csv

--- db_builder.py
+++ db_builder.py
@@ -18,6 +18,8 @@ if __name__ == '__main__':
     with Database('test.db', debug=True) as db:
         db.add_csv('peeps.csv', types=('TEXT', 'INT', 'INT PRIMARY KEY'))
 
+        db.add_csv('courses.csv', types=('TEXT', 'INT', 'INT'))
+
         dir_path = 'baseball/'
         for csv in os.listdir(dir_path):
             db.add_csv(dir_path + csv)
